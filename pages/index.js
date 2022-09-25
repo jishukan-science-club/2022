51a32d230209cdbc255f760b0cbb8c12b5a1beed
Zect3279 <zeck.3279@gmail.com>
2022-09-25 23:08:35 +0900

    initial

---
+++ pages/index.js
@@ -0,0 +1,56 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.stage}>
+        <div className={styles.logo}>
+          <div className={styles.jssclogo} />
+          <div className={styles.jslogo} />
+        </div>
+        <nav className={styles.nav}>
+          <Link href="/exp">
+            <div className={styles.inner}>
+              <p>Experiments</p>
+            </div>
+          </Link>
+          <Link href="/note">
+            <div className={styles.inner}>
+              <p>Notes</p>
+            </div>
+          </Link>
+          <Link href="/ext">
+            <div className={styles.inner}>
+              <p>Ext</p>
+            </div>
+          </Link>
+          <Link href="/about">
+            <div className={styles.inner}>
+              <p>About</p>
+            </div>
+          </Link>
+        </nav>
+        {/* <div className={styles.strf}>
+          <div className={styles.naf}>
+            <div />
+            <div />
+            <div />
+            <div />
+            <div />
+            <div />
+            <div />
+            <div />
+            <div />
+          </div>
+        </div> */}
+        <div className={styles.skelton}>
+          <div />
+          <div />
+          <div />
+        </div>
+      </div>
+    </div>
+  )
+}
