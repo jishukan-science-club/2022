71cfae81c90ae03fedbbf077e2f32fcbf47cf830
Zect3279 <Zect3279@users.noreply.github.com>
2022-10-04 15:07:39 +0000

    deploy: 5860dcbb27135e2e7402d7fd931433fe13d14b13

--- _next/static/chunks/pages/ext-60796140ac26d348.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[986],{5553:function(n,u,t){(window.__NEXT_P=window.__NEXT_P||[]).push(["/ext",function(){return t(3921)}])},3921:function(n,u,t){"use strict";t.r(u);var _=t(5893);u.default=function(){return(0,_.jsx)("div",{children:"Ext"})}}},function(n){n.O(0,[774,888,179],function(){return n(n.s=5553)}),_N_E=n.O()}]);
